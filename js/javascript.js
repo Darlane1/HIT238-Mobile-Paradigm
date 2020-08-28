548295ea009a77119478034f77af838101b07509
Darlane T <ling_1489@hotmail.com>
2020-08-28 16:53:19 +0930

    success js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,7 @@
+var txt = '{"name":"Northern Territory","caseno":"idk"}'
+var obj = JSON.parse(txt);
+document.getElementById("iii").innerHTML = obj.name;
+
 function showcase(){
   if(document.getElementById('state').value == '1'){
     document.getElementById('showcases').innerHTML="like 0 lol";}
@@ -15,7 +19,9 @@ function showcase(){
 
 function opennav(){
 if (document.getElementById('insidenav').style.width == '250px')
-  document.getElementById('insidenav').style.width = "0px";
+  {document.getElementById('insidenav').style.width = "0px";
+  document.body.style.backgroundColor = "white";}
 else {
-  document.getElementById('insidenav').style.width = "250px";}
+  document.getElementById('insidenav').style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";}
 }
