f2c69af1b7c0aea741f821557ce91e6a42b2a54b
Darlane T <ling_1489@hotmail.com>
2020-08-29 12:15:55 +0930

    update, added placeholder img and more js stuff

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,45 @@
-var txt = '{"name":"Northern Territory","caseno":"idk"}'
-var obj = JSON.parse(txt);
-document.getElementById("iii").innerHTML = obj.name;
+var australia = {
+  "nt":{
+    "state":"Northern Territory",
+    "caseno":"1"
+  },
+  "qld":{
+    "state":"Queensland",
+    "caseno":"3"
+  },
+  "sa":{
+    "state":"South Australia",
+    "caseno":"21"
+  },
+  "wa":{
+    "state":"Western Australia",
+    "caseno":"94"
+  },
+  "vic":{
+    "state":"Victoria",
+    "caseno":"4,628"
+  },
+  "nsw":{
+    "state":"New South Wales",
+    "caseno":"1,471"
+  }
+};
+
 
 function showcase(){
-  if(document.getElementById('state').value == '1'){
-    document.getElementById('showcases').innerHTML="like 0 lol";}
-  else if (document.getElementById('state').value == '2'){
-    document.getElementById('showcases').innerHTML="idk maybe 4";}
-  else if (document.getElementById('state').value == '3'){
-    document.getElementById('showcases').innerHTML="like a lot";}
-  else if (document.getElementById('state').value == '4'){
-    document.getElementById('showcases').innerHTML="ye idk";}
-  else if (document.getElementById('state').value == '5'){
-    document.getElementById('showcases').innerHTML="who?";}
-  else if (document.getElementById('state').value == '6'){
-    document.getElementById('showcases').innerHTML="10,000";}
+  document.getElementsByClassName("circle")[0].style.display="block";
+  if(document.getElementById('state').value == '1' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.nt.caseno; document.getElementById("state_name").innerHTML = australia.nt.state;}
+  else if (document.getElementById('state').value == '2' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.qld.caseno; document.getElementById("state_name").innerHTML = australia.qld.state;}
+  else if (document.getElementById('state').value == '3' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.vic.caseno; document.getElementById("state_name").innerHTML = australia.vic.state;}
+  else if (document.getElementById('state').value == '4' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.nsw.caseno; document.getElementById("state_name").innerHTML = australia.nsw.state;}
+  else if (document.getElementById('state').value == '5' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.wa.caseno; document.getElementById("state_name").innerHTML = australia.wa.state;}
+  else if (document.getElementById('state').value == '6' && document.getElementById('country').value == '1'){
+    document.getElementById('showcases').innerHTML=australia.sa.caseno; document.getElementById("state_name").innerHTML = australia.sa.state;}
 }
 
 function opennav(){
