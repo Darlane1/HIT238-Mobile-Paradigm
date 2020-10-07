3f0971efc4ce89e471a1c2a5427b726de54e618c
Darlane1 <ling_1489@hotmail.com>
2020-10-07 22:52:30 +0930

    changed placement of service worker to be above fetch

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
+// service worker //
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+};
 //Attempting to fetch data.json//
 var myInit = {
   method: 'GET',
@@ -18,19 +30,6 @@ fetch (myRequest)
     console.log(data);
     let data_ = data;
   });
-
-// service worker //
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-};
 
 //Injecting html using javascript//
 //With the help of: https://css-tricks.com/template-literals/ //
--- js/module.js
+++ js/module.js
@@ -3,7 +3,7 @@ window.onload = function() {
   var countUp = new CountUp('target', 2000);
   countUp.start();
 };
-let demo = new CountUp('count_anim', 6447);
+let demo = new CountUp('count_anim', 6969);
 if (!demo.error) {
   demo.start();
 } else {
